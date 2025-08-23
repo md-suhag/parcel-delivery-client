333f9308ea6b26f2359d124abfdc33b22e2162e6
md-suhag <abdussalamsuhag104@gmail.com>
2025-08-23 09:21:44 +0600

    add: navbar

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Navbar from "./Navbar";
 
 interface IProps {
   children: ReactNode;
@@ -7,7 +8,7 @@ interface IProps {
 export default function CommonLayout({ children }: IProps) {
   return (
     <div className=" min-h-screen flex flex-col">
-      <nav>nav</nav>
+      <Navbar />
       <main className="grow-1">{children}</main>
       <footer>footer</footer>
     </div>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,8 @@
 import App from "@/App";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import Faq from "@/pages/Faq";
+import Features from "@/pages/Features";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
@@ -12,6 +16,22 @@ export const router = createBrowserRouter([
       {
         Component: Home,
         index: true,
+      },
+      {
+        Component: Features,
+        path: "/features",
+      },
+      {
+        Component: About,
+        path: "/about",
+      },
+      {
+        Component: Faq,
+        path: "/faq",
+      },
+      {
+        Component: Contact,
+        path: "/contact",
       },
     ],
   },
